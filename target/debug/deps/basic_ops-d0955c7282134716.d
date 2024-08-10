3c201346c756489e41884f6014c98bd2b95384f7
marcellobeltrami <marcello.beltrami@gmail.com>
2024-08-10 17:07:05 +0100

    quick-save

--- target/debug/deps/basic_ops-d0955c7282134716.d
+++ target/debug/deps/basic_ops-d0955c7282134716.d
@@ -1,5 +1,5 @@
-/home/marcello/Rust_projects/machine_learning/target/debug/deps/libbasic_ops-d0955c7282134716.rmeta: tests/basic_ops.rs
+/home/marcello/Rust_ML/target/debug/deps/libbasic_ops-d0955c7282134716.rmeta: tests/basic_ops.rs
 
-/home/marcello/Rust_projects/machine_learning/target/debug/deps/basic_ops-d0955c7282134716.d: tests/basic_ops.rs
+/home/marcello/Rust_ML/target/debug/deps/basic_ops-d0955c7282134716.d: tests/basic_ops.rs
 
 tests/basic_ops.rs:
